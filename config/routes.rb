e39fa7426a6deacaf8406d40480d3dfe80449cae
olkhon <alexander.stapel@gmail.com>
2016-06-19 23:24:37 +0200

    Durch die Erstellung des ersten Controllers wurden die config-Routes geändert.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/impressum'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/login'
+
+  get 'static_pages/registrieren'
+
+  get 'static_pages/bewerten'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
