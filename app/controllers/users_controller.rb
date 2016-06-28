70685e7df9bd3b8d6ba8bc3c9a4783c4e17ef0cc
olkhon <alexander.stapel@gmail.com>
2016-06-28 10:09:00 +0200

    Daten Controller angelegt, hintergrundbild home eingefügt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+ before_action :logged_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -37,6 +39,12 @@ class UsersController < ApplicationController
   end
 
 
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
   private
 
